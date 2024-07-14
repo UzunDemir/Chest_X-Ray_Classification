9ac3eb0f67a811596d1c28edc456bd33c8354bef
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2024-07-14 18:08:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -118,6 +118,19 @@ from io import BytesIO
 # Настройка страницы Streamlit в самом начале скрипта
 st.set_page_config(page_title="Chest X-ray Classifier", layout="wide")
 
+hide_github_icon = """
+<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK { 
+    display: none !important; 
+}
+#MainMenu, footer, header { 
+    display: none !important; 
+}
+</style>
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 st.divider()
 
